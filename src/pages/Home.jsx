6cbd4e5cb1f4228a96875c8fb3b8ef580601c8b4
JinDaifer <thedaifer1@gmail.com>
2025-08-04 10:50:55 +0200

    Add routing and basic pages for DishNow app
    Integrated react-router-dom and created AppRouter for navigation. Added Home, Login, Register, and NotFound pages with placeholder content. Updated index.html with new favicon and title, and replaced App component content to use routing. Added logo and favicon assets.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Bienvenido a la página de inicio</h1>;
+};
+
+export default Home;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,5 @@
+const Register = () => {
+  return <h1>Bienvenido a la página de register</h1>;
+};
+
+export default Register;
