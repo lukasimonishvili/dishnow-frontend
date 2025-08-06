d90db8561408b6a90b1b34d599fcae14d3316b2f
JinDaifer <thedaifer1@gmail.com>
2025-08-06 09:56:39 +0200

    Add reusable Button component and update Home panel
    Introduced a styled Button component for consistent button styling and replaced raw button elements in HomeDescriptionPanel with the new component. Also added styled section for improved layout and maintainability.

---
+++ src/components/Button.jsx
@@ -0,0 +1,31 @@
+import Styled from "styled-components";
+
+
+const StyledButton = Styled.button`
+    background: ${(props) => (props.color)};
+    border-radius: 6px;
+    
+    font-family: 'Manrope';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 22px;
+    /* identical to box height */
+    letter-spacing: -0.02em;
+
+    color: ${(props) => (props.text_color)};
+    padding-bottom: 13px;
+    padding-top: 13px;
+    padding-right: 33px;
+    padding-left: 33px;
+`;
+
+const Button = ({ input_text, color, text_color, on_click_function }) => {
+
+    return <>
+    <StyledButton color={color} text_color={text_color} onClick={on_click_function}>{input_text}</StyledButton>
+    </>
+
+};
+
+export default Button;
--- src/components/HomeDescriptionPanel.jsx
+++ src/components/HomeDescriptionPanel.jsx
@@ -1,10 +1,25 @@
 import woman_home from "../assets/img/home_woman.png";
 import Styled from "styled-components";
+import Button from "./Button";
+
+
+const StyledSection = Styled.section`
+  background-color: #ffffff00;
+  border-top: 0.5px #3C3C3C solid;
+  border-bottom: 0.5px #3C3C3C solid;
+`;
 
 const HomeDescriptionPanel = () => {
 
+  const ExploreHandle = () => {
+    
+  };
+  const LogInHandle = () => {
+    
+  };
+
   return <>
-    <section>
+    <StyledSection>
       <div>
         <h4>TRY TODAY</h4>
         <h1>
@@ -14,8 +29,8 @@ const HomeDescriptionPanel = () => {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
 
         <div>
-          <button type="button">Explore</button>
-          <button type="button">Log in</button>
+          <Button input_text="Explore" color="#FC8800" text_color="#ffffff" on_click_function={ExploreHandle}/>
+          <Button input_text="Log in" color="#ffffff00" text_color="#000000" on_click_function={LogInHandle}/>
         </div>
 
       </div>
@@ -23,7 +38,7 @@ const HomeDescriptionPanel = () => {
 
       <img src={woman_home} alt="cooking woman" />
 
-    </section>
+    </StyledSection>
 
   </>
 };
